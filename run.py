dcf00f15d6a77e17d81ad368a9db6648d62042f5
Aaron Chambers <aaronchambers@pop-os.localdomain>
2020-04-12 16:09:17 -0700

    cleaned up code, fixed resolution issue, now saves normals and saves in godot dictionary as well

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 from PIL import Image
 import json
-from os import listdir
+import os
 
-image_size = 1024
-tile_size = 64
+
+image_size = 512
+half_tile_size = 32
+
+main_path = "../godot_farm/assets/textures/"
 
 
 def crop(image_name):
@@ -11,27 +14,40 @@ def crop(image_name):
 	width  = image_params["w"]
 	height = image_params["h"]
 	frames = image_params["frames"]
+	output_path = image_params["outputPath"]
 
-	left  = image_size/2 - tile_size * width
-	right = image_size/2 + tile_size * width
+	left  = image_size/2 - half_tile_size * width
+	right = image_size/2 + half_tile_size * width
 
-	top    = image_size - tile_size * height*2
+	top    = image_size - half_tile_size * height*2
 	bottom = image_size
 
-	image_width = tile_size * width
-	image_height = tile_size * height
+	image_width = half_tile_size * width
+	image_height = half_tile_size * height
 
 	image_out = Image.new('RGBA', (image_width * 2, image_height * 2 * frames))
+	image_out_normal = Image.new('RGBA', (image_width * 2, image_height * 2 * frames))
 
 	for i in range(0, frames):
 		image = Image.open(r"./input/" + image_name + "/output/" + image_name + str(i) + ".png") 
 
 		cropped_image = image.crop((left, top, right, bottom)) 
 		image_out.paste(cropped_image, (0, image_height * 2 * i))
-		#cropped_image.save("./output/" + image_name + "/" + image_name + str(i) + ".png")
 
-	image_out.save("./output/" + image_name + "/" + image_name + ".png")
+		image_normal = Image.open(r"./input/" + image_name + "/output/normals/" + image_name + str(i) + ".png")
+		cropped_image_normal = image_normal.crop((left, top, right, bottom))
+		image_out_normal.paste(cropped_image_normal, (0, image_height * 2 * i))
 
+	saveImage('./output/' + image_name + "/", image_name, image_out)
+	saveImage('./output/' + image_name + "/normals/", image_name, image_out_normal)
+
+	saveImage(main_path + output_path, image_name, image_out)
+	saveImage(main_path + output_path + "normals/", image_name, image_out_normal)
+
+def saveImage(path, fileName, image):
+	if not os.path.exists(path):
+		os.makedirs(path)
+	image.save(path + image_name + ".png")
 
 with open('images.json') as json_file:
 	params = json.load(json_file)
